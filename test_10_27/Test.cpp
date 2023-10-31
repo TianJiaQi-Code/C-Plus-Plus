ee3365a3bf690b441e83bf5aba79cd9581adae4f
TianJiaQi <2780888910@qq.com>
2023-10-31 22:57:06 +0800

    C++入门

--- test_10_27/Test.cpp
+++ test_10_27/Test.cpp
@@ -336,22 +336,131 @@ using namespace std;
 //	return 0;
 //}
 
-// F.h
-#include <iostream>
-using namespace std;
-inline void f(int i);
-// F.cpp
-#include "F.h"
-void f(int i)
+//// F.h
+//#include <iostream>
+//using namespace std;
+//inline void f(int i);
+//// F.cpp
+//#include "F.h"
+//void f(int i)
+//{
+//	cout << i << endl;
+//}
+//// main.cpp
+//#include "F.h"
+//int main()
+//{
+//	f(10);
+//	return 0;
+//}
+//// 链接错误：main.obj : error LNK2019: 无法解析的外部符号 "void __cdecl
+//f(int)" (?f@@YAXH@Z)，该符号在函数 _main 中被引用
+
+//#include <string>
+//#include <map>
+//int main()
+//{
+//	std::map<std::string, std::string> m{ { "apple", "苹果" }, { "orange",
+//	"橙子" }, { "pear","梨" } };
+//	std::map<std::string, std::string>::iterator it = m.begin();
+//	while (it != m.end())
+//	{
+//		//....
+//	}
+//	return 0;
+//}
+
+//#include <string>
+//#include <map>
+//typedef std::map<std::string, std::string> Map;
+//int main()
+//{
+//	Map m{ { "apple", "苹果" },{ "orange", "橙子" }, {"pear","梨"} };
+//	Map::iterator it = m.begin();
+//	while (it != m.end())
+//	{
+//		//....
+//	}
+//	return 0;
+//}
+
+//typedef char* pstring;
+//int main()
+//{
+//	const pstring p1; // 编译成功还是失败？
+//	const pstring* p2; // 编译成功还是失败？
+//	return 0;
+//}
+
+//int TestAuto()
+//{
+//	return 10;
+//}
+//int main()
+//{
+//	int a = 10;
+//	auto b = a;
+//	auto c = 'a';
+//	auto d = TestAuto();
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;
+//	cout << typeid(d).name() << endl;
+//	//auto e; 无法通过编译，使用auto定义变量时必须对其进行初始化
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x = 10;
+//	auto a = &x;
+//	auto* b = &x;
+//	auto& c = x;
+//	cout << typeid(a).name() << endl;
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;
+//	*a = 20;
+//	*b = 30;
+//	c = 40;
+//	return 0;
+//}
+
+//void TestAuto()
+//{
+//	auto a = 1, b = 2;
+//	auto c = 3, d = 4.0; // 该行代码会编译失败，因为c和d的初始化表达式类型不同
+//}
+
+//// 此处代码编译失败，auto不能作为形参类型，因为编译器无法对a的实际类型进行推导
+//void TestAuto(auto a)
+//{}
+
+//void TestAuto()
+//{
+//	int a[] = { 1,2,3 };
+//	auto b[] = { 4，5，6 };
+//}
+
+//void TestFor()
+//{
+//	int array[] = { 1, 2, 3, 4, 5 };
+//	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+//		array[i] *= 2;
+//	for (int* p = array; p < array + sizeof(array) / sizeof(array[0]); ++p)
+//		cout << *p << endl;
+//}
+
+//void TestFor()
+//{
+//	int array[] = { 1, 2, 3, 4, 5 };
+//	for (auto& e : array)
+//		e *= 2;
+//	for (auto e : array)
+//		cout << e << " ";
+//	return 0;
+//}
+
+void TestFor(int array[])
 {
-	cout << i << endl;
+	for (auto& e : array)
+		cout << e << endl;
 }
-// main.cpp
-#include "F.h"
-int main()
-{
-	f(10);
-	return 0;
-}
-// 链接错误：main.obj : error LNK2019: 无法解析的外部符号 "void __cdecl
-f(int)" (?f@@YAXH@Z)，该符号在函数 _main 中被引用
