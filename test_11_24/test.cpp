07b3b751d84d4927a5c0112ba233929afe1d272d
TianJiaQi <2780888910@qq.com>
2023-11-25 22:48:16 +0800

    类与对象 中

--- test_11_24/test.cpp
+++ test_11_24/test.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
-
+class Date
+{
+public:
+	void Init(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void Print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
 int main()
 {
+	Date d1;
+	d1.Init(2022, 7, 5);
+	d1.Print();
+	Date d2;
+	d2.Init(2022, 7, 6);
+	d2.Print();
 	return 0;
 }
