b71371b7588486ee6cec5e995f0ed13ea918ef71
TianJiaQi <2780888910@qq.com>
2023-11-26 22:05:00 +0800

    类与对象 中

--- test_11_24/test.cpp
+++ test_11_24/test.cpp
@@ -1,28 +1,153 @@
-class Date
+#include <iostream>
+using namespace std;
+
+//class Date {};
+
+//class Date
+//{
+//public:
+//	void Init(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	d1.Init(2022, 7, 5);
+//	d1.Print();
+//	Date d2;
+//	d2.Init(2022, 7, 6);
+//	d2.Print();
+//	return 0;
+//}
+
+//class Date
+//{
+//public:
+//	// 1.无参构造函数
+//	Date()
+//	{}
+//	// 2.带参构造函数
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void TestDate()
+//{
+//	Date d1; // 调用无参构造函数
+//	Date d2(2015, 1, 1); // 调用带参的构造函数
+//	// 注意：如果通过无参构造函数创建对象时，对象后面不用跟括号，否则就成了函数声明
+//	// 以下代码的函数：声明了d3函数，该函数无参，返回一个日期类型的对象
+//	// warning C4930: “Date d3(void)”: 未调用原型函数(是否是有意用变量定义的?)
+//	Date d3();
+//}
+
+//class Date
+//{
+//public:
+//	/*
+//	// 如果用户显式定义了构造函数，编译器将不再生成
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	*/
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	// 将Date类中构造函数屏蔽后，代码可以通过编译，因为编译器生成了一个无参的默认构造函数。
+//	// 将Date类中构造函数放开，代码编译失败，因为一旦显式定义任何构造函数，编译器将不再生成无参构造函数，
+//	// 放开后报错：error C2512: “Date”: 没有合适的默认构造函数可用。
+//	Date d1;
+//	return 0;
+//}
+
+//class Time
+//{
+//public:
+//	Time()
+//	{
+//		cout << "Time()" << endl;
+//		_hour = 0;
+//		_minute = 0;
+//		_second = 0;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//private:
+//	// 基本类型(内置类型)
+//	int _year;
+//	int _month;
+//	int _day;
+//	// 自定义类型
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d;
+//	return 0;
+//}
+
+class Time
 {
 public:
-	void Init(int year, int month, int day)
+	Time()
 	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-	void Print()
-	{
-		cout << _year << "-" << _month << "-" << _day << endl;
+		cout << "Time()" << endl;
+		_hour = 0;
+		_minute = 0;
+		_second = 0;
 	}
 private:
-	int _year;
-	int _month;
-	int _day;
+	int _hour;
+	int _minute;
+	int _second;
+};
+class Date
+{
+private:
+	// 基本类型(内置类型)
+	int _year = 1970;
+	int _month = 1;
+	int _day = 1;
+	// 自定义类型
+	Time _t;
 };
 int main()
 {
-	Date d1;
-	d1.Init(2022, 7, 5);
-	d1.Print();
-	Date d2;
-	d2.Init(2022, 7, 6);
-	d2.Print();
+	Date d;
 	return 0;
 }
