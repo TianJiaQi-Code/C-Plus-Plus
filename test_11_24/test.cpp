331c858688ef3bd37e6b498798f92cd7985833b1
TianJiaQi <2780888910@qq.com>
2023-11-30 22:43:19 +0800

    类与对象 中

--- test_11_24/test.cpp
+++ test_11_24/test.cpp
@@ -196,40 +196,191 @@ using namespace std;
 //	s.Push(2);
 //}
 
-class Time
-{
-public:
-	~Time()
-	{
-		cout << "~Time()" << endl;
-	}
-private:
-	int _hour;
-	int _minute;
-	int _second;
-};
+//class Time
+//{
+//public:
+//	~Time()
+//	{
+//		cout << "~Time()" << endl;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//private:
+//	// 基本类型(内置类型)
+//	int _year = 1970;
+//	int _month = 1;
+//	int _day = 1;
+//	// 自定义类型
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d;
+//	return 0;
+//}
+
+/*
+ 程序运行结束后输出：~Time()
+ 在main方法中根本没有直接创建Time类的对象，为什么最后会调用Time类的析构函数？
+ 因为：main方法中创建了Date对象d，而d中包含4个成员变量，其中_year, _month,_day三个是
+ 内置类型成员，销毁时不需要资源清理，最后系统直接将其内存回收即可；而_t是Time类对象，所以在
+ d销毁时，要将其内部包含的Time类的_t对象销毁，所以要调用Time类的析构函数。但是：main函数
+ 中不能直接调用Time类的析构函数，实际要释放的是Date类对象，所以编译器会调用Date类的析构函
+ 数，而Date没有显式提供，则编译器会给Date类生成一个默认的析构函数，目的是在其内部调用Time
+ 类的析构函数，即当Date对象销毁时，要保证其内部每个自定义对象都可以正确销毁
+ main函数中并没有直接调用Time类析构函数，而是显式调用编译器为Date类生成的默认析构函数
+ 注意：创建哪个类的对象则调用该类的析构函数，销毁那个类的对象则调用该类的析构函数
+*/
+
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	// Date(const Date& d) // 正确写法
+//	Date(const Date d) // 错误写法：编译报错，会引发无穷递归
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	Date d2(d1);
+//	return 0;
+//}
+
+//class Time
+//{
+//public:
+//	Time()
+//	{
+//		_hour = 1;
+//		_minute = 1;
+//		_second = 1;
+//	}
+//	Time(const Time& t)
+//	{
+//		_hour = t._hour;
+//		_minute = t._minute;
+//		_second = t._second;
+//		cout << "Time::Time(const Time&)" << endl;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//private:
+//	// 基本类型(内置类型)
+//	int _year = 1970;
+//	int _month = 1;
+//	int _day = 1;
+//	// 自定义类型
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d1;
+//	// 用已经存在的d1拷贝构造d2，此处会调用Date类的拷贝构造函数
+//	// 但Date类并没有显式定义拷贝构造函数，则编译器会给Date类生成一个默认的拷贝构造函数
+//	Date d2(d1);
+//	return 0;
+//}
+
+//// 这里会发现下面的程序会崩溃掉？这里就需要我们以后讲的深拷贝去解决。
+//typedef int DataType;
+//class Stack
+//{
+//public:
+//	Stack(size_t capacity = 10)
+//	{
+//		_array = (DataType*)malloc(capacity * sizeof(DataType));
+//		if (nullptr == _array)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		_size = 0;
+//		_capacity = capacity;
+//	}
+//	void Push(const DataType& data)
+//	{
+//		// CheckCapacity();
+//		_array[_size] = data;
+//		_size++;
+//	}
+//	~Stack()
+//	{
+//		if (_array)
+//		{
+//			free(_array);
+//			_array = nullptr;
+//			_capacity = 0;
+//			_size = 0;
+//		}
+//	}
+//private:
+//	DataType* _array;
+//	size_t _size;
+//	size_t _capacity;
+//};
+//int main()
+//{
+//	Stack s1;
+//	s1.Push(1);
+//	s1.Push(2);
+//	s1.Push(3);
+//	s1.Push(4);
+//	Stack s2(s1);
+//	return 0;
+//}
+
 class Date
 {
+public:
+	Date(int year, int minute, int day)
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+	Date(const Date& d)
+	{
+		cout << "Date(const Date& d):" << this << endl;
+	}
+	~Date()
+	{
+		cout << "~Date():" << this << endl;
+	}
 private:
-	// 基本类型(内置类型)
-	int _year = 1970;
-	int _month = 1;
-	int _day = 1;
-	// 自定义类型
-	Time _t;
+	int _year;
+	int _month;
+	int _day;
 };
+Date Test(Date d)
+{
+	Date temp(d);
+	return temp;
+}
 int main()
 {
-	Date d;
+	Date d1(2022, 1, 13);
+	Test(d1);
 	return 0;
 }
-// 程序运行结束后输出：~Time()
-// 在main方法中根本没有直接创建Time类的对象，为什么最后会调用Time类的析构函数？
-// 因为：main方法中创建了Date对象d，而d中包含4个成员变量，其中_year, _month,_day三个是
-// 内置类型成员，销毁时不需要资源清理，最后系统直接将其内存回收即可；而_t是Time类对象，所以在
-// d销毁时，要将其内部包含的Time类的_t对象销毁，所以要调用Time类的析构函数。但是：main函数
-// 中不能直接调用Time类的析构函数，实际要释放的是Date类对象，所以编译器会调用Date类的析构函
-// 数，而Date没有显式提供，则编译器会给Date类生成一个默认的析构函数，目的是在其内部调用Time
-// 类的析构函数，即当Date对象销毁时，要保证其内部每个自定义对象都可以正确销毁
-// main函数中并没有直接调用Time类析构函数，而是显式调用编译器为Date类生成的默认析构函数
-// 注意：创建哪个类的对象则调用该类的析构函数，销毁那个类的对象则调用该类的析构函数
