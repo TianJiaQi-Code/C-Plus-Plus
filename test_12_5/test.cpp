9b23bf872ae95ef64da0d5dcd54240f09dfa684e
TianJiaQi <2780888910@qq.com>
2023-12-08 22:57:49 +0800

    类与对象 下

--- test_12_5/test.cpp
+++ test_12_5/test.cpp
@@ -1,14 +1,98 @@
-class Date
+#include <iostream>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a(a)
+//	{}
+//private:
+//	int _a;
+//};
+//class B
+//{
+//public:
+//	B(int a, int ref)
+//		:_aobj(a)
+//		, _ref(ref)
+//		, _n(10)
+//	{}
+//private:
+//	A _aobj; // 没有默认构造函数
+//	int& _ref; // 引用
+//	const int _n; // const
+//};
+
+//class Time
+//{
+//public:
+//	Time(int hour = 0)
+//		:_hour(hour)
+//	{
+//		cout << "Time()" << endl;
+//	}
+//private:
+//	int _hour;
+//};
+//class Date
+//{
+//public:
+//	Date(int day)
+//	{}
+//private:
+//	int _day;
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d(1);
+//}
+
+class A
 {
 public:
-	Date(int year, int month, int day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
+	A(int a)
+		:_a1(a)
+		, _a2(_a1)
+	{}
+	void Print() {
+		cout << _a1 << " " << _a2 << endl;
 	}
 private:
-	int _year;
-	int _month;
-	int _day;
+	int _a2;
+	int _a1;
 };
+int main() {
+	A aa(1);
+	aa.Print();
+}
