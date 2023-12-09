2deef7d901001743ad9ec2e3daa7dfe21ea6fca8
TianJiaQi <2780888910@qq.com>
2023-12-09 23:03:40 +0800

    类与对象 下

--- test_12_5/test.cpp
+++ test_12_5/test.cpp
@@ -78,21 +78,63 @@ using namespace std;
 //	Date d(1);
 //}
 
-class A
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a1(a)
+//		, _a2(_a1)
+//	{}
+//	void Print() {
+//		cout << _a1 << " " << _a2 << endl;
+//	}
+//private:
+//	int _a2;
+//	int _a1;
+//};
+//int main() {
+//	A aa(1);
+//	aa.Print();
+//}
+
+class Date
 {
 public:
-	A(int a)
-		:_a1(a)
-		, _a2(_a1)
+	// 1. 单参构造函数，没有使用explicit修饰，具有类型转换作用
+// explicit修饰构造函数，禁止类型转换---explicit去掉之后，代码可以通过编译
+	explicit Date(int year)
+		:_year(year)
 	{}
-	void Print() {
-		cout << _a1 << " " << _a2 << endl;
+	/*
+	// 2. 虽然有多个参数，但是创建对象时后两个参数可以不传递，没有使用explicit修饰，具
+	有类型转换作用
+	// explicit修饰构造函数，禁止类型转换
+	explicit Date(int year, int month = 1, int day = 1)
+	: _year(year)
+	, _month(month)
+	, _day(day)
+	{}
+	*/
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
+		return *this;
 	}
 private:
-	int _a2;
-	int _a1;
+	int _year;
+	int _month;
+	int _day;
 };
-int main() {
-	A aa(1);
-	aa.Print();
+void Test()
+{
+	Date d1(2022);
+	// 用一个整形变量给日期类型对象赋值
+	// 实际编译器背后会用2023构造一个无名对象，最后用无名对象给d1对象进行赋值
+	d1 = 2023;
+	// 将1屏蔽掉，2放开时则编译失败，因为explicit修饰构造函数，禁止了单参构造函数类型转换的作用
 }
