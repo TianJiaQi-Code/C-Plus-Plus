20e58db1a7060be2d7a4b040e8b160f5f828a9f0
TianJiaQi <2780888910@qq.com>
2023-12-11 22:36:31 +0800

    类与对象 下

--- test_12_5/test.cpp
+++ test_12_5/test.cpp
@@ -97,44 +97,136 @@ using namespace std;
 //	aa.Print();
 //}
 
+//class Date
+//{
+//public:
+//	// 1. 单参构造函数，没有使用explicit修饰，具有类型转换作用
+//// explicit修饰构造函数，禁止类型转换---explicit去掉之后，代码可以通过编译
+//	explicit Date(int year)
+//		:_year(year)
+//	{}
+//	/*
+//	// 2. 虽然有多个参数，但是创建对象时后两个参数可以不传递，没有使用explicit修饰，具
+//	有类型转换作用
+//	// explicit修饰构造函数，禁止类型转换
+//	explicit Date(int year, int month = 1, int day = 1)
+//	: _year(year)
+//	, _month(month)
+//	, _day(day)
+//	{}
+//	*/
+//	Date& operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//		return *this;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void Test()
+//{
+//	Date d1(2022);
+//	// 用一个整形变量给日期类型对象赋值
+//	// 实际编译器背后会用2023构造一个无名对象，最后用无名对象给d1对象进行赋值
+//	d1 = 2023;
+//	// 将1屏蔽掉，2放开时则编译失败，因为explicit修饰构造函数，禁止了单参构造函数类型转换的作用
+//}
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//	// d1 << cout; -> d1.operator<<(&d1, cout); 不符合常规调用
+//// 因为成员函数第一个参数一定是隐藏的this，所以d1必须放在<<的左侧
+//	ostream& operator<<(ostream& _cout)
+//	{
+//		_cout << _year << "-" << _month << "-" << _day << endl;
+//		return _cout;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//class Date
+//{
+//	friend ostream& operator<<(ostream& _cout, const Date& d);
+//	friend istream& operator>>(istream& _cin, Date& d);
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//ostream& operator<<(ostream& _cout, const Date& d)
+//{
+//	_cout << d._year << "-" << d._month << "-" << d._day;
+//	return _cout;
+//}
+//istream& operator>>(istream& _cin, Date& d)
+//{
+//	_cin >> d._year;
+//	_cin >> d._month;
+//	_cin >> d._day;
+//	return _cin;
+//}
+//int main()
+//{
+//	Date d;
+//	cin >> d;
+//	cout << d << endl;
+//	return 0;
+//}
+
+class Time
+{
+	friend class Date; // 声明日期类为时间类的友元类，则在日期类中就直接访问Time类中的私有成员变量
+public:
+	Time(int hour = 0, int minute = 0, int second = 0)
+		: _hour(hour)
+		, _minute(minute)
+		, _second(second)
+	{}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
 class Date
 {
 public:
-	// 1. 单参构造函数，没有使用explicit修饰，具有类型转换作用
-// explicit修饰构造函数，禁止类型转换---explicit去掉之后，代码可以通过编译
-	explicit Date(int year)
-		:_year(year)
+	Date(int year = 1900, int month = 1, int day = 1)
+		: _year(year)
+		, _month(month)
+		, _day(day)
 	{}
-	/*
-	// 2. 虽然有多个参数，但是创建对象时后两个参数可以不传递，没有使用explicit修饰，具
-	有类型转换作用
-	// explicit修饰构造函数，禁止类型转换
-	explicit Date(int year, int month = 1, int day = 1)
-	: _year(year)
-	, _month(month)
-	, _day(day)
-	{}
-	*/
-	Date& operator=(const Date& d)
+	void SetTimeOfDate(int hour, int minute, int second)
 	{
-		if (this != &d)
-		{
-			_year = d._year;
-			_month = d._month;
-			_day = d._day;
-		}
-		return *this;
+		// 直接访问时间类私有的成员变量
+		_t._hour = hour;
+		_t._minute = minute;
+		_t._second = second;
 	}
 private:
 	int _year;
 	int _month;
 	int _day;
+	Time _t;
 };
-void Test()
-{
-	Date d1(2022);
-	// 用一个整形变量给日期类型对象赋值
-	// 实际编译器背后会用2023构造一个无名对象，最后用无名对象给d1对象进行赋值
-	d1 = 2023;
-	// 将1屏蔽掉，2放开时则编译失败，因为explicit修饰构造函数，禁止了单参构造函数类型转换的作用
-}
