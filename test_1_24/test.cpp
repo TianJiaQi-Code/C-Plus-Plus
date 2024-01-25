6ab92fbb1a0bf5570891f5b99e93c8473eac11ab
TianJiaQi <2780888910@qq.com>
2024-01-25 20:25:47 +0800

    1-23 OJ

--- test_1_24/test.cpp
+++ test_1_24/test.cpp
@@ -52,30 +52,73 @@
 //    cout << count;
 //}
 
+// 排序子序列
 #include <iostream>
-#include <string>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    string str;
-    getline(cin, str);
+int main() {
+    // 输入
+    int n = 0;
+    cin >> n;
 
-    reverse(str.begin(), str.end());
-
-    int left = 0;
-    int right = 0;
-    while (1)
-    {
-        right = str.find(' ', left);
-        if (right == -1)
-            break;
-
-        reverse(str.begin() + left, str.begin() + right);
-
-        left = right + 1;
+    vector<int> v;
+    v.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    // 划分
+    int count = 0;
+    int i = 0;
+    while (i < n) {
+        if (v[i] < v[i + 1]) {
+            while (i + 1 < n && v[i] <= v[i + 1]) {
+                i++;
+            }
+            count++;
+            i++;
+        }
+        else if (v[i] == v[i + 1]) {
+            i++;
+        }
+        else if (v[i] > v[i + 1]) {
+            while (i + 1 < n && v[i] >= v[i + 1]) {
+                i++;
+            }
+            count++;
+            i++;
+        }
     }
 
-    cout << str;
+    // 输出
+    cout << count;
 }
+
+
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//    string str;
+//    getline(cin, str);
+//
+//    reverse(str.begin(), str.end());
+//
+//    int left = 0;
+//    int right = 0;
+//    while (1)
+//    {
+//        right = str.find(' ', left);
+//        if (right == -1)
+//            break;
+//
+//        reverse(str.begin() + left, str.begin() + right);
+//
+//        left = right + 1;
+//    }
+//
+//    cout << str;
+//}
