ece043057079ad277e8869c799760cbbc4a95724
TianJiaQi <2780888910@qq.com>
2024-02-20 23:18:18 +0800

    继承

--- test_2_17/test.cpp
+++ test_2_17/test.cpp
@@ -39,23 +39,109 @@ using namespace std;
 //	return 0;
 //}
 
-// 实例演示三种继承关系下基类成员的各类型成员访问关系的变化
-class Person
+//// 实例演示三种继承关系下基类成员的各类型成员访问关系的变化
+//class Person
+//{
+//public:
+//	void Print()
+//	{
+//		cout << _name << endl;
+//	}
+//protected:
+//	string _name; // 姓名
+//private:
+//	int _age; // 年龄
+//};
+////class Student : protected Person
+////class Student : private Person
+//class Student : public Person
+//{
+//protected:
+//	int _stunum; // 学号
+//};
+
+//class Person
+//{
+//protected:
+//	string _name; // 姓名
+//	string _sex; // 性别
+//	int _age; // 年龄
+//};
+//class Student : public Person
+//{
+//public:
+//	int _No; // 学号
+//};
+//void Test()
+//{
+//	Student sobj;
+//	// 1.子类对象可以赋值给父类对象/指针/引用
+//	Person pobj = sobj;
+//	Person* pp = &sobj;
+//	Person& rp = sobj;
+//	//2.基类对象不能赋值给派生类对象
+//	sobj = pobj;
+//	// 3.基类的指针可以通过强制类型转换赋值给派生类的指针
+//	pp = &sobj;
+//	Student * ps1 = (Student*)pp; // 这种情况转换时可以的。
+//	ps1->_No = 10;
+//	pp = &pobj;
+//	Student* ps2 = (Student*)pp; // 这种情况转换时虽然可以，但是会存在越界访问的问题
+//	ps2->_No = 10;
+//}
+
+//// Student的_num和Person的_num构成隐藏关系，可以看出这样代码虽然能跑，但是非常容易混淆
+//class Person
+//{
+//protected:
+//	string _name = "小杨"; // 姓名
+//	int _num = 111; // 身份证号
+//};
+//class Student : public Person
+//{
+//public:
+//	void Print()
+//	{
+//		cout << "姓名:" << _name << endl;
+//		cout << "身份证号:" << Person::_num << endl;
+//		cout << "学号:" << _num << endl;
+//	}
+//protected:
+//	int _num = 999; // 学号
+//};
+//void Test()
+//{
+//	Student s1;
+//	s1.Print();
+//};
+
+// B中的fun和A中的fun不是构成重载，因为不是在同一作用域
+// B中的fun和A中的fun构成隐藏，成员函数满足函数名相同就构成隐藏。
+class A
 {
 public:
-	void Print()
+	void fun()
 	{
-		cout << _name << endl;
+		cout << "func()" << endl;
 	}
-protected:
-	string _name; // 姓名
-private:
-	int _age; // 年龄
 };
-//class Student : protected Person
-//class Student : private Person
-class Student : public Person
+class B : public A
 {
-protected:
-	int _stunum; // 学号
+public:
+	void fun(int i)
+	{
+		A::fun();
+		cout << "func(int i)->" << i << endl;
+	}
 };
+void Test()
+{
+	B b;
+	b.fun(10);
+};
+
+int main()
+{
+	Test();
+	return 0;
+}
