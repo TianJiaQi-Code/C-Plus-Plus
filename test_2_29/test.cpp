7e5cfe5a8273fd8be115efca3d72939803033d3d
TianJiaQi <2780888910@qq.com>
2024-02-29 19:38:17 +0800

    2-29 OJ

---
+++ test_2_29/test.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int input;
+//    while (cin >> input)
+//    {
+//        int count = 0;
+//        for (int i = 0; i < 32; i++)
+//        {
+//            if (((input >> i) & 1) == 1)
+//            {
+//                count++;
+//            }
+//        }
+//        cout << count << endl;
+//    }
+//    return 0;
+//}
+
