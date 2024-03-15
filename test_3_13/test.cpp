8b85b2aa9fb491b8bf41f6a8512b5505f49e192a
TianJiaQi <2780888910@qq.com>
2024-03-15 23:00:03 +0800

    二叉树 OJ

--- test_3_13/test.cpp
+++ test_3_13/test.cpp
@@ -15,3 +15,34 @@
 //        return to_string(root->val) + "(" + tree2str(root->left) + ")(" + tree2str(root->right) + ")";
 //    }
 //};
+
+//class Solution
+//{
+//public:
+//    vector<vector<int>> levelOrder(TreeNode* root)
+//    {
+//        queue<TreeNode*> que;
+//        vector<vector<int>> res;
+//
+//        if (root != nullptr)
+//            que.push(root);
+//
+//        while (!que.empty())
+//        {
+//            vector<int> tmp;
+//            for (int i = que.size(); i > 0; i--)
+//            {
+//                root = que.front();
+//                que.pop();
+//                tmp.push_back(root->val);
+//
+//                if (root->left != nullptr)
+//                    que.push(root->left);
+//                if (root->right != nullptr)
+//                    que.push(root->right);
+//            }
+//            res.push_back(tmp);
+//        }
+//        return res;
+//    }
+//};
