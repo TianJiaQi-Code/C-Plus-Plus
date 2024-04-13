5992d44bb824070e84e0a9f4dbf84eee1cb2404a
TianJiaQi <2780888910@qq.com>
2024-04-13 23:09:48 +0800

    哈希底层

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -70,8 +70,95 @@ namespace open_address
 			return nullptr;
 		}
 
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (Find(kv.first))
+				return false;
+
+			// 扩容的问题
+			if (_n * 10 / _tables.size() >= 7)
+			{
+				HashTable<K, V, Hash> newHT(_tables.size() * 2);
+				// 遍历旧表，插入到新表
+				for (auto e : _tables)
+				{
+					if (e._state == EXIST)
+					{
+						newHT.Insert(e._kv);
+					}
+				}
+				_tables.swap(newHT._tables);
+			}
+
+			Hash hs;
+			// 线性探测
+			size_t hashi = hs(kv.first) % _tables.size();
+			while (_tables[hashi]._state == EXIST)
+			{
+				++hashi;
+				hashi %= _tables.size();
+			}
+
+			_tables[hashi]._kv = kv;
+			_tables[hashi]._state = EXIST;
+			++_n;
+
+			return true;
+		}
+
+		bool Erase(const K& key)
+		{
+			HashData<K, V>* ret = Find(key);
+			if (ret)
+			{
+				_n--;
+				ret->_state = DELETE;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
 	private:
 		vector<HashData<K, V>> _tables;
 		size_t _n = 0;	// 实际存储的数据个数
+	};
+}
+
+namespace hash_bucket
+{
+	template<class K, class V>
+	struct HashNode
+	{
+		HashNode<K, V>* _next;
+		pair<K, V> _kv;
+	};
+
+	template<class K, class V>
+	class HashTable
+	{
+		typedef HashNode<K, V> Node;
+	public:
+		HashTable()
+		{
+			_tables.resize(10, nullptr);
+			_n = 0;
+		}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			size_t hashi = kv.first % tables.size();
+			Node* newnode = new Node(kv);
+
+			// 头插
+			newnode->_next = _tables[hashi];
+			_tables[hashi] = newnode;
+		}
+
+	private:
+		vector<Node*> _tables;	// 指针数组
+		size_t n;
 	};
 }
--- HashTable/Test.cpp
+++ HashTable/Test.cpp
@@ -1,11 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 #include "HashTable.h"
 
+namespace open_address
+{
+	void TestHT1()
+	{
+		int a[] = { 1,4,24,34,7,44,17,37 };
+		HashTable<int, int> ht;
+		for (auto e : a)
+		{
+			ht.Insert(make_pair(e, e));
+		}
+
+		for (auto e : a)
+		{
+			auto ret = ht.Find(e);
+			if (ret)
+			{
+				cout << ret->_kv.first << ":E" << endl;
+			}
+			else
+			{
+				cout << e << ":D" << endl;
+			}
+		}
+		cout << endl;
+
+		ht.Erase(34);
+		ht.Erase(4);
+		for (auto e : a)
+		{
+			auto ret = ht.Find(e);
+			if (ret)
+			{
+				cout << ret->_kv.first << ":E" << endl;
+			}
+			else
+			{
+				cout << e << ":D" << endl;
+			}
+		}
+	}
+
+	void TestHT2()
+	{
+		HashTable<string, string> dict;
+		dict.Insert(make_pair("sort", "ÅÅÐò"));
+		dict.Insert(make_pair("string", "×Ö·û´®"));
+	}
+}
+
 int main()
 {
-
+	//open_address::TestHT1();
+	open_address::TestHT2();
 	return 0;
 }
