a977d60f0fceeb78e3fdd98c38e6972466cdf780
TianJiaQi <2780888910@qq.com>
2024-04-24 22:02:44 +0800

    C++11

---
+++ test_4_24/Test.cpp
@@ -0,0 +1,233 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//struct Point
+//{
+//	int _x;
+//	int _y;
+//};
+//int main()
+//{
+//	int array1[] = { 1, 2, 3, 4, 5 };
+//	int array2[5] = { 0 };
+//	Point p = { 1, 2 };
+//	return 0;
+//}
+
+//struct Point
+//{
+//	int _x;
+//	int _y;
+//};
+//int main()
+//{
+//	int x1 = 1;
+//	int x2{ 2 };
+//	int array1[]{ 1, 2, 3, 4, 5 };
+//	int array2[5]{ 0 };
+//	Point p{ 1, 2 };
+//	// C++11中列表初始化也可以适用于new表达式中
+//	int* pa = new int[4] { 0 };
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{
+//		cout << "Date(int year, int month, int day)" << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1(2022, 1, 1); // old style
+//	// C++11支持的列表初始化，这里会调用构造函数初始化
+//	Date d2{ 2022, 1, 2 };
+//	Date d3 = { 2022, 1, 3 };
+//	return 0;
+//}
+
+//int main()
+//{
+//	// the type of il is an initializer_list
+//	auto il = { 10, 20, 30 };
+//	cout << typeid(il).name() << endl;
+//	return 0;
+//}
+
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+
+//namespace tjq
+//{
+//	template<class T>
+//	class vector
+//	{
+//	public:
+//		typedef T* iterator;
+//
+//		vector(initializer_list<T> l)
+//		{
+//			_start = new T[l.size()];
+//			_finish = _start + l.size();
+//			_endofstorage = _start + l.size();
+//
+//			iterator vit = _start;
+//			typename initializer_list<T>::iterator lit = l.begin();
+//			while (lit != l.end())
+//			{
+//				*vit++ = *lit++;
+//			}
+//		}
+//
+//		vector<T>& operator=(initializer_list<T> l)
+//		{
+//			vector<T> tmp(l);
+//			std::swap(_start, tmp._start);
+//			std::swap(_finish, tmp._finish);
+//			std::swap(_endofstorage, tmp._endofstorage);
+//
+//			return *this;
+//		}
+//
+//	private:
+//		iterator _start;
+//		iterator _finish;
+//		iterator _endofstorage;
+//	};
+//}
+//
+//int main()
+//{
+//	vector<int> v = { 1,2,3,4 };
+//
+//	tjq::vector<int> v1{1, 2, 3, 4};
+//	tjq::vector<int> v2 = { 1,2,3,4 };
+//
+//
+//	list<int> lt = { 1,2 };
+//	// 这里{"sort", "排序"}会先初始化构造一个pair对象
+//	map<string, string> dict = { {"sort", "排序"}, {"insert", "插入"} };
+//	// 使用大括号对容器赋值
+//	v = { 10, 20, 30 };
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 10;
+//	auto p = &i;
+//	auto pf = strcpy;
+//	cout << typeid(p).name() << endl;
+//	cout << typeid(pf).name() << endl;
+//	map<string, string> dict = { {"sort", "排序"}, {"insert", "插入"} };
+//	//map<string, string>::iterator it = dict.begin();
+//	auto it = dict.begin();
+//	return 0;
+//}
+
+//// decltype的一些使用使用场景
+//template<class T1, class T2>
+//void F(T1 t1, T2 t2)
+//{
+//	decltype(t1 * t2) ret;
+//	cout << typeid(ret).name() << endl;
+//}
+//int main()
+//{
+//	const int x = 1;
+//	double y = 2.2;
+//	decltype(x * y) ret; // ret的类型是double
+//	decltype(&x) p; // p的类型是int*
+//	cout << typeid(ret).name() << endl;
+//	cout << typeid(p).name() << endl;
+//	F(1, 'a');
+//	return 0;
+//}
+
+//#ifndef NULL
+//#ifdef __cplusplus
+//#define NULL 0
+//#else
+//#define NULL ((void *)0)
+//#endif
+//#endif
+
+//int main()
+//{
+//	// 以下的p、b、c、*p都是左值
+//	int* p = new int(0);
+//	int b = 1;
+//	const int c = 2;
+//	// 以下几个是对上面左值的左值引用
+//	int*& rp = p;
+//	int& rb = b;
+//	const int& rc = c;
+//	int& pvalue = *p;
+//	return 0;
+//}
+
+//int main()
+//{
+//	double x = 1.1, y = 2.2;
+//	// 以下几个都是常见的右值
+//	10;
+//	x + y;
+//	fmin(x, y);
+//	// 以下几个都是对右值的右值引用
+//	int&& rr1 = 10;
+//	double&& rr2 = x + y;
+//	double&& rr3 = fmin(x, y);
+//	// 这里编译会报错：error C2106: “=”: 左操作数必须为左值
+//	10 = 1;
+//	x + y = 1;
+//	fmin(x, y) = 1;
+//	return 0;
+//}
+
+//int main()
+//{
+//	double x = 1.1, y = 2.2;
+//	int&& rr1 = 10;
+//	const double&& rr2 = x + y;
+//	rr1 = 20;
+//	rr2 = 5.5; // 报错
+//	return 0;
+//}
+
+//int main()
+//{
+//	// 左值引用只能引用左值，不能引用右值。
+//	int a = 10;
+//	int& ra1 = a; // ra为a的别名
+//	//int& ra2 = 10; // 编译失败，因为10是右值
+//	// const左值引用既可引用左值，也可引用右值。
+//	const int& ra3 = 10;
+//	const int& ra4 = a;
+//	return 0;
+//}
+
+int main()
+{
+	// 右值引用只能右值，不能引用左值。
+	int&& r1 = 10;
+	// error C2440: “初始化”: 无法从“int”转换为“int &&”
+	// message : 无法将左值绑定到右值引用
+	int a = 10;
+	int&& r2 = a;
+	// 右值引用可以引用move以后的左值
+	int&& r3 = std::move(a);
+	return 0;
+}
