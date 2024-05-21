686f2f1f4b69ca4952e0496238b37ce5f2f24347
TianJiaQi <2780888910@qq.com>
2024-05-21 20:48:19 +0800

    2024.5.16-OJ

--- test_5_16/test.cpp
+++ test_5_16/test.cpp
@@ -1,62 +1,75 @@
 // 2024-5-16
 
-// 1.
-//#include <iostream>
-//#include <list>
-//using namespace std;
-//
-//void Reverse(int& n, list<int>& lst, int& left, int& right)
-//{
-//    list<int> rlst;
-//
-//    auto it = lst.begin();
-//    for (int i = 0; i < right; i++)
-//    {
-//        // 使用临时变量避免迭代器失效：如果迭代器指向元素被删除，迭代器就会失效
-//        auto tmpit = it;
-//        ++it;
-//
-//        if (i >= left - 1)
-//        {
-//            // void splice (iterator position, list& x, iterator i);
-//            rlst.splice(rlst.begin(), lst, tmpit);
-//        }
-//    }
-//
-//    // 把反转后的子链表拼接回主链表
-//    it = lst.begin();
-//    for (int i = 0; i < left - 1; i++)
-//    {
-//        ++it;
-//    }
-//    // void splice (iterator position, list& x);
-//    lst.splice(it, rlst);
-//}
-//
-//int main()
-//{
-//    int n = 0;
-//    while (cin >> n)
-//    {
-//        list<int> lst;
-//        int left = 0, right = 0;
-//        for (int i = 0; i < n; i++)
-//        {
-//            int tmp = 0;
-//            cin >> tmp;
-//            lst.push_back(tmp);
-//        }
-//        cin >> left >> right;
-//
-//        Reverse(n, lst, left, right);
-//        for (auto& e : lst)
-//        {
-//            cout << e << ' ';
-//        }
-//        cout << endl;
-//    }
-//
-//    return 0;
-//}
+// 1. 反转部分单向链表
+#include <iostream>
+#include <list>
+using namespace std;
+
+void Reverse(const int& n, list<int>& lst, const int& left, const int& right)
+{
+    list<int> rlst;
+
+    auto it = lst.begin();
+    for (int i = 0; i < right; i++)
+    {
+        // 使用临时变量避免迭代器失效：如果迭代器指向元素被删除，迭代器就会失效
+        auto tmpit = it;
+        ++it;
+
+        if (i >= left - 1)
+        {
+            // void splice (iterator position, list& x, iterator i);
+            rlst.splice(rlst.begin(), lst, tmpit);
+        }
+    }
+
+    // 把反转后的子链表拼接回主链表
+    it = lst.begin();
+    for (int i = 0; i < left - 1; i++)
+    {
+        ++it;
+    }
+    // void splice (iterator position, list& x);
+    lst.splice(it, rlst);
+}
+
+int main()
+{
+    int n = 0;
+    while (cin >> n)
+    {
+        list<int> lst;
+        int left = 0, right = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int tmp = 0;
+            cin >> tmp;
+            lst.push_back(tmp);
+        }
+        cin >> left >> right;
+
+        Reverse(n, lst, left, right);
+        for (auto& e : lst)
+        {
+            cout << e << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
 // 2. 猴子分桃
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    while (cin >> n && n != 0)
+    {
+        cout << (long long)pow(5, n) - 4 << ' ' << (long long)pow(4, n) + n - 4 << endl;
+    }
+    return 0;
+}
